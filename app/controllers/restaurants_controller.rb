420a313e62a792a7aa521b22a2bd89dda59b3175
stefan22 <stefan.emailacct@gmail.com>
2015-07-31 16:52:22 +0100

    adds update method and passes test

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -24,6 +24,11 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to '/restaurants'
+  end
 
 
 end
