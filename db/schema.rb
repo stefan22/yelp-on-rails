e6756505decffb50a3a5e84e90eed4eb84718e62
stefan22 <stefan.emailacct@gmail.com>
2015-08-02 17:34:23 +0100

    modified db schema

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,9 @@ ActiveRecord::Schema.define(version: 20150802160639) do
   create_table "sevenelevens", force: :cascade do |t|
     t.string   "name"
     t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
