2ab4047d7ab14cc539a3736f64a2bff6b2201ed1
stefan22 <stefan.emailacct@gmail.com>
2015-08-04 00:17:57 +0100

    spec models restaurant

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe Restaurant, type: :model do
+describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
+
+  
+
+
+
+
+
+
+
 end
