f0fcb6ad5bfedf7de7d9505f244773ada227c4ce
stefan22 <stefan.emailacct@gmail.com>
2015-08-04 23:30:18 +0100

    test for uniqueness

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -10,7 +10,11 @@ describe Restaurant, type: :model do
     expect(restaurant).not_to be_valid
   end
 
-
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Moe's tavern")
+    restaurant = Restaurant.new(name:"Moe's tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 
 
 
