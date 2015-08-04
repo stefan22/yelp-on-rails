364901e4768adcacfa51059f981566a2b7359ff4
stefan22 <stefan.emailacct@gmail.com>
2015-08-04 21:32:23 +0100

    spec restaurant model test

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,6 +6,7 @@ describe Restaurant, type: :model do
 
   it 'is not valid with a name of less than three characters' do
     restaurant = Restaurant.new(name:'kf')
+    expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
 
