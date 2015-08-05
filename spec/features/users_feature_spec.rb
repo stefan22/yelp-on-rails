f30fd5b5296b688d08fe7cb6931ba634fe5676ed
stefan22 <stefan.emailacct@gmail.com>
2015-08-05 22:54:38 +0100

    sign out link feature test

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -24,6 +24,17 @@ feature 'User can sign in and out' do
       click_button 'Sign up'
     end
 
+    it "should see a 'sign out' link" do
+      visit '/'
+      expect(page).to have_link 'Sign out'
+    end
+
+    it 'should not see a sign in link and a sign up link' do
+      visit '/'
+      expect(page).not_to have_link 'Sign in'
+      expect(page).not_to have_link 'Sign up'
+    end
+
   end
 
 
