e8f21adecd1e6a7f43fcc099694655acd377371e
stefan22 <stefan.emailacct@gmail.com>
2015-08-06 18:46:55 +0100

    fixes user test password length

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -19,8 +19,8 @@ feature 'User can sign in and out' do
       visit '/'
       click_link 'Sign up'
       fill_in 'Email', with: 'test@test.com'
-      fill_in 'Password', with: 'test'
-      fill_in 'Password confirmation', with: 'test'
+      fill_in 'Password', with: 'testtest'
+      fill_in 'Password confirmation', with: 'testtest'
       click_button 'Sign up'
     end
 
