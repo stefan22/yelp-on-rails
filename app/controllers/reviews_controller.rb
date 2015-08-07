0cbe00657d981fba870972db1f8a04bd7f110039
stefan22 <stefan.emailacct@gmail.com>
2015-08-07 01:13:41 +0100

    adds before action to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, :except => [:index, :show]
+
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
