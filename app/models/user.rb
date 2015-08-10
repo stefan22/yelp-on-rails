e9e8e540e2dfca379a6f472fb35c15422c0e18ce
stefan22 <stefan.emailacct@gmail.com>
2015-08-10 19:53:57 +0100

    adds has reviewed method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_many :restaurants
-has_many :reviews
-has_many :reviewed_restaurants, through: :reviews
+  has_many :restaurants
+  has_many :reviews
+  has_many :reviewed_restaurants, through: :reviews, source: :restaturant
+
+  def has_reviewed?(restaurant)
+    reviewed_restaurants.include? restaurant
+  end
+
+
 
 end
