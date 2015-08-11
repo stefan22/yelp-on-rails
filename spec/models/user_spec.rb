b79f5aebbdc756f91fc791e65f066f47fa86fb96
stefan22 <stefan.emailacct@gmail.com>
2015-08-11 19:00:18 +0100

    removes reviewed restaurants test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,3 @@ require 'rails_helper'
 #RSpec.describe User, type: :model do
 #  pending "add some examples to (or delete) #{__FILE__}"
 #end
-
-describe User, type: :model do
-  it { is_expected.to have_many :reviewed_restaurants }
-
-end
